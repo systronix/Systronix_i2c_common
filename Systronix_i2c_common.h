ecf5e92e193bd11a450c166cbbdf5e4590249240
systronix <bboyes@systronix.com>
2018-10-02 13:17:15 -0600

    recognize Teensy 3.5, 3.6

--- Systronix_i2c_common.h
+++ Systronix_i2c_common.h
@@ -9,7 +9,8 @@
 
 #include <Arduino.h>
 
-#if defined (__MK20DX256__) || defined (__MK20DX128__) 	// Teensy 3.1 or 3.2 || Teensy 3.0
+#if defined (__MK20DX128__) || defined(__MK20DX256__) || defined(__MKL26Z64__) || \
+                           defined(__MK64FX512__) || defined(__MK66FX1M0__) // 3.0/3.1-3.2/LC/3.5/3.6
 #include <i2c_t3.h>	// for Teensy 3 family use optimized and more robust library
 #else
 #include <Wire.h>	// for AVR I2C library
